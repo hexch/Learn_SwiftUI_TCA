5194385dce08098c2ba98fae2a028d2b33efb6e6
hexch <hexch.c@gmail.com>
2022-01-28 11:17:19 +0900

    Initial Commit

---
+++ LearnSwiftUITCA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnSwiftUITCA
+//
+//  Created by XIAOCHUAN HE on R 4/01/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnSwiftUITCA/LearnSwiftUITCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUITCAApp.swift
+//  LearnSwiftUITCA
+//
+//  Created by XIAOCHUAN HE on R 4/01/28.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUITCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
